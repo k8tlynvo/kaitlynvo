609151393bd3ab6919daaa0ad6203db0e4634698
k8tlynvo <kqvo@ucdavis.edu>
2024-10-11 00:00:00 -0700

    file name change

--- app/(components)/NavBar.tsx
+++ app/(components)/NavBar.tsx
@@ -9,7 +9,7 @@ const NavBar = () => {
                 </Link>
             </div>
             <div className='transition duration-300 hover:text-pink-300 hover:underline hover:decoration-pink-200'>
-                <a href='/pdfs/resume.pdf' target='_blank' rel='noopener noreferrer'>
+                <a href='/pdfs/public_resume.pdf' target='_blank' rel='noopener noreferrer'>
                     Resume 
                 </a>
             </div>
